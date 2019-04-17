6b76e5a94965a73d4138ea94dda4a9ff2a536eb7
rishav08 <rishavkumar08@gmail.com>
2019-04-17 02:24:19 -0500

    added url length threshold, check for url occurrences, social link removal and http 404 error fix

--- crawler_producer.py
+++ crawler_producer.py
@@ -16,9 +16,24 @@ def getLinks(url):
     response.close()
 
     soup = BeautifulSoup(content, "html.parser")
-    links = []
-    for link in soup.findAll('a', attrs={'href': re.compile("^/")}):
-            links.append(url + link.get('href'))
+    links = {}
+    minLen = 50 # Setting the threshold for the minimum length of url
+    social = ["facebook.com", "twitter.com", "instagram.com", "plus.google.com", "linkedin.com", "youtube.com", "pinterest.com", "behance.net", "blog", "pdf"] # Excluding urls containing these
+    flag = 1
+    for link in soup.findAll('a', attrs={'href': re.compile("^((https?://)|/)")}):
+        if len(link.get('href')) > minLen:
+            if "http" in link.get('href') or "www" in link.get('href'):
+                finalURL = link.get('href')
+            else:
+                finalURL = url + link.get('href')
+            for l in social:
+                if l in finalURL:
+                    flag = 0
+            if links.get(finalURL) and flag == 1:
+                links[finalURL] = links.get(finalURL) + 1
+            elif flag == 1:
+                links[finalURL] = 1
+            flag = 1
 
     return links
 
@@ -34,7 +49,7 @@ if __name__ == "__main__":
 
     for link in links:
         if link:
-            print(link)
+            # print(link)
             try:  # need to open with try
                 list = getLinks(link)
             except urllib.error.HTTPError as e:
@@ -43,14 +58,20 @@ if __name__ == "__main__":
                 raise  # if other than 404, raise the error
 
             for item in list:
-                print(item)
-                article = getData(item)
-                print("Url: "+ str(article.url))
-                print("Headline: "+ str(article.title))
-                print("authors: "+ str(article.authors))
-                print("Lead Paragraph: "+ str(article.description))
-                print("Text: "+ str(article.text))
-                print("Data Published: "+ str(article.date_publish))
-                print("")
+                if (list[item] == 1): # checking the number of occurrence of the link in the website
+                    # print(item)
+                    try:  # need to open with try
+                        article = getData(item)
+                    except urllib.error.HTTPError as e:
+                        if e.getcode() == 404:  # check the return code
+                            continue
+                        raise  # if other than 404, raise the error
+                    print("Url: "+ str(article.url))
+                    print("Headline: "+ str(article.title))
+                    print("authors: "+ str(article.authors))
+                    print("Lead Paragraph: "+ str(article.description))
+                    print("Text: "+ str(article.text))
+                    print("Data Published: "+ str(article.date_publish))
+                    print("")
             print("")
 
